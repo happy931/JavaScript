a4492cc00cec4c6481212b5dc485d7f12acd32a7
happy931 <66877590+happy931@users.noreply.github.com>
2024-10-08 05:39:27 +0000

    BMI_Calculaor Completed

--- DOM/Change_BG_Color/BMI_Calculator/main.js
+++ DOM/Change_BG_Color/BMI_Calculator/main.js
@@ -1,5 +1,5 @@
 document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+    event.preventDefault();   // to stop default action of the form
     
     const height = parseFloat(document.getElementById('height').value);
     const weight = parseFloat(document.getElementById('weight').value);
@@ -7,7 +7,20 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
 
     if (height > 0 && weight > 0) {
         const bmi = (weight / ((height / 100) ** 2)).toFixed(2);
-        message = `Your BMI is ${bmi}.`;
+        
+        if(bmi < 18.5)
+            {
+            message = `You are UNDERWEIGHT and your BMI is ${bmi}.`;          
+        }
+        else if(bmi > 30)
+            {
+            message = `You are OBESE and your BMI is ${bmi}.`;   
+            
+        }
+        else{
+            message = `You're weight is normal and your BMI is ${bmi}.`;           
+            
+        }
     } else {
         message = 'Please enter valid height and weight.';
     }
